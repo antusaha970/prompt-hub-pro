3219d7b2b011ed480caddbcd26cdc00d2f4961a2
Antu Saha <antusaha970@gmail.com>
2023-06-15 20:31:15 +0600

    added model to save prompt

--- app/create-prompt/page.js
+++ app/create-prompt/page.js
@@ -7,11 +7,34 @@ import Form from "@components/Form/Form";
 
 const CreatePrompt = () => {
   const [submitting, setSubmitting] = useState(false);
+  const router = useRouter();
+  const { data: session } = useSession();
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user?.id,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log({ error });
+    } finally {
+      setSubmitting(false);
+    }
+  };
   return (
     <Form
       type="Create"
