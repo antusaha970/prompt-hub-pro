31ef08f20e99adb1dccd5ea69e93c1f59dd70575
Antu Saha <antusaha970@gmail.com>
2023-06-17 14:21:50 +0600

    changed logo

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -18,13 +18,6 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2">
-        <Image
-          src="/assets/images/logo.svg"
-          alt="Promptopia Logo"
-          width={30}
-          height={30}
-          className="object-contain"
-        />
         <p className="logo_text">Prompt Hub</p>
       </Link>
       {/* {Destop navigation} */}
