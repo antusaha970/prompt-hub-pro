2d606ebd04992a3ae442e0609ea0fb480b773476
Antu Saha <antusaha970@gmail.com>
2023-06-18 23:14:36 +0600

    tring to fix the bug

--- models/prompt.js
+++ models/prompt.js
@@ -1,5 +1,24 @@
 import { Schema, model, models } from "mongoose";
-
+import mongoose from "mongoose";
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, "Email already exists"],
+    require: [true, "Email is required"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+    ],
+  },
+  image: {
+    type: String,
+  },
+});
+mongoose.model("User", UserSchema);
 const PromptSchema = new Schema({
   creator: {
     type: Schema.Types.ObjectId,
