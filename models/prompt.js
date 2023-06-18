b7b78dcd17625f2d340cc6283b33ae6292d9e989
Antu Saha <antusaha970@gmail.com>
2023-06-18 23:23:58 +0600

    change the ref to users

--- models/prompt.js
+++ models/prompt.js
@@ -1,28 +1,9 @@
 import { Schema, model, models } from "mongoose";
-import mongoose from "mongoose";
-const UserSchema = new Schema({
-  email: {
-    type: String,
-    unique: [true, "Email already exists"],
-    require: [true, "Email is required"],
-  },
-  username: {
-    type: String,
-    required: [true, "Username is required!"],
-    match: [
-      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
-    ],
-  },
-  image: {
-    type: String,
-  },
-});
-const User = models.User || mongoose.model("User", UserSchema);
+
 const PromptSchema = new Schema({
   creator: {
     type: Schema.Types.ObjectId,
-    ref: "User",
+    ref: "users",
   },
   prompt: {
     type: String,
